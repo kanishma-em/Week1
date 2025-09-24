8bbc03009fd6eb3705c6d462403e1194ba3609a3
Kanishma M <kanishmamanikandan518@gmail.com>
2025-09-24 16:16:03 +0530

    Add files via upload

---
+++ prb8.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class prb8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n>0) {
+            System.out.println("positive");
+            
+        }
+        else if (n==0)
+        {
+            System.out.println("zero");
+        }
+        else{
+            System.out.println("negative");
+        }
+
+    }
+}
