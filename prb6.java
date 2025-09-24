8a3cc9ba9524508a9de561bab4cc775d84308803
Kanishma M <kanishmamanikandan518@gmail.com>
2025-09-24 15:45:57 +0530

    Add files via upload

---
+++ prb6.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class prb6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a > b) {
+            System.out.println("Max=" + a + " Min=" + b);
+        }
+        else {
+            System.out.println("Max=" + b + " Min=" + a);
+        }
+
+        sc.close();
+
+    }
+}
